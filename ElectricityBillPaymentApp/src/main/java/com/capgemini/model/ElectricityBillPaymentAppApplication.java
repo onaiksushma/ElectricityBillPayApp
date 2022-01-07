15c377184e3ebf5e2f8876bda9e9f5a689858f6d
SUMIT <SUMIT@LAPTOP-IQ9IAIUC>
2022-01-07 14:08:37 +0530

    First Commit from Sushma

---
+++ ElectricityBillPaymentApp/src/main/java/com/capgemini/model/ElectricityBillPaymentAppApplication.java
@@ -0,0 +1,15 @@
+package com.capgemini.model;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElectricityBillPaymentAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElectricityBillPaymentAppApplication.class, args);
+		
+		System.out.println("entry from sushma");
+	}
+
+}
